373311a9077513b9b3ffd29d49df2f993c115700
hilyas <hilyas@ufl.edu>
2021-10-22 18:07:29 -0400

    part 2

---
+++ Assignment3/part2.R
@@ -0,0 +1,47 @@
+library(DESeq2)
+library(ggplot2)
+library(magrittr)
+library(ggalluvial)
+countData <- Data[-c(1)]
+dds <- DESeqDataSetFromMatrix(countData = countData, colData = colnames, design=~type)
+
+deseq_object <- DESeq(dds)
+
+deseq_results = results(deseq_object)
+deseq_results <- lfcShrink(
+  deseq_object,
+  coef = 2,
+  res = deseq_results
+)
+
+deseq_results
+
+deseq_df <- deseq_results %>%
+  as.data.frame() %>%
+  tibble::rownames_to_column("Gene") %>%
+  dplyr::mutate(threshold = padj < 0.05) %>%
+  dplyr::arrange(dplyr::desc(log2FoldChange))
+
+deseq_df
+
+subset = deseq_df[1:5000,]
+
+dist_subset = dist(subset)
+hclust_subset = hclust(dist_subset)
+plot(hclust_subset)
+
+subset10 = deseq_df[1:10,]
+subset100 = deseq_df[1:100,]
+subset1000 = deseq_df[1:1000,]
+subset10000 = deseq_df[1:10000,]
+
+dist_subset10 = dist(subset10)
+dist_subset100 = dist(subset100)
+dist_subset1000 = dist(subset1000)
+dist_subset10000 = dist(subset10000)
+
+hclust_subset10 = hclust(dist_subset10)
+hclust_subset100 = hclust(dist_subset100)
+hclust_subset1000 = hclust(dist_subset1000)
+hclust_subset10000 = hclust(dist_subset10000)
+
